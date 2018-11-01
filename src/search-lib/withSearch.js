e9a47be03b818cc4af03ed31307882a39021927b
Jason Stoltzfus <jastoltz24@gmail.com>
2018-11-01 10:40:55 -0400

    Added "mapContextToProps" to "withSearch"
    This is so that we have a hook into state so that we can transform
    data before passing them on as props to a component. Helpful if
    you want to do something like apply a custom sort to data, etc.

--- src/search-lib/withSearch.js
+++ src/search-lib/withSearch.js
@@ -6,12 +6,17 @@ import SearchConsumer from "./SearchConsumer";
  * This is a Higher Order Component that is used to expose (as `props`) all
  * state and Actions provided by the SearchProvider to "container"
  * components.
+ *
+ * `mapContextToProps` can be used to manipulate actions and state from context
+ * before they are passed on to the container.
  */
 export default function withSearch(Component) {
   return function(props) {
+    const { mapContextToProps = context => context, ...rest } = props;
+
     return (
       <SearchConsumer>
-        {context => <Component {...context} {...props} />}
+        {context => <Component {...mapContextToProps(context)} {...rest} />}
       </SearchConsumer>
     );
   };
