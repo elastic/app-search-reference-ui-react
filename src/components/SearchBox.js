03728cd5cd1a4f3652cacd30c10dff632aaa6021
Jason Stoltzfus <jastoltz24@gmail.com>
2019-01-30 12:46:41 -0500

    SearchBox support for suggest

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,25 +2,49 @@ import PropTypes from "prop-types";
 import React from "react";
 
 function SearchBox(props) {
-  const { isFocused, inputProps, onChange, onSubmit, value } = props;
+  const {
+    isFocused,
+    inputProps,
+    onChange,
+    onSelectSuggestion,
+    onSubmit,
+    showSuggestions,
+    suggestions = [],
+    value
+  } = props;
   const focusedClass = isFocused ? "focus" : "";
 
   return (
-    <form className="searchbox" onSubmit={onSubmit}>
-      <input
-        className={`searchbox__text-input ${focusedClass}`}
-        onChange={onChange}
-        type="text"
-        value={value}
-        placeholder="Search your documents&#8230;"
-        {...inputProps}
-      />
-      <input
-        type="submit"
-        value="Search"
-        className="button searchbox__submit"
-      />
-    </form>
+    <React.Fragment>
+      <form className="searchbox" onSubmit={onSubmit}>
+        <input
+          className={`searchbox__text-input ${focusedClass}`}
+          onChange={onChange}
+          type="text"
+          value={value}
+          placeholder="Search your documents&#8230;"
+          {...inputProps}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="button searchbox__submit"
+        />
+      </form>
+      <div>
+        {/* Using mousedown instead of click to ensure it has higher priority than
+        blur handler*/}
+        {showSuggestions &&
+          suggestions.map(suggestion => (
+            <div
+              onMouseDown={() => onSelectSuggestion(suggestion)}
+              key={suggestion.suggestion}
+            >
+              {suggestion.suggestion}
+            </div>
+          ))}
+      </div>
+    </React.Fragment>
   );
 }
 
